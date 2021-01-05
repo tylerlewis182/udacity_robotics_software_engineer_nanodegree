9a84d8b1831eafe5d32648b960e2c55cbe399aaa
Tyler Lewis <tylerlewis182@gmail.com>
2021-01-04 16:15:22 -0800

    cleaned up comments

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -4,8 +4,12 @@
 #include <string>
 #include <typeinfo>
 
+
+
 // Define a global client that can request services
 ros::ServiceClient client;
+
+
 
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
@@ -24,7 +28,6 @@ void drive_robot(float lin_x, float ang_z)
 
 
 
-
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
@@ -36,12 +39,11 @@ void process_image_callback(const sensor_msgs::Image img)
 
     int num_white_pixels_found = 0;
     int x_bar_sum = 0;
-    int x_bar = -1;
-    float m = -2.0 / (float)cols;
+    int x_bar = -1; // area centroid of the white ball
+    float m = -2.0 / (float)cols; // used to transform x_bar [0, 2400] into a valid ang_z value [1, -1]
 
     float lin_x = 0;
     float ang_z = 0;
-
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
@@ -50,18 +52,18 @@ void process_image_callback(const sensor_msgs::Image img)
 
     for(int i=0; i<rows; i++) // 800 rows  
     {
-        for(int j=0; j<cols; j+=3) // 2400 columns, by 3's (800 effective collumns)
+        for(int j=0; j<cols; j+=3) // 2400 columns, by 3's
         {
             int index = (i * cols) + j;
             if(index < 0 or index + 2 >= n)
             {
-                ROS_ERROR("ERROR! Index out of rangefrom within the 'drive_robot' function in ball_chaser package, 'process_image.cpp' file.");
+                ROS_ERROR("ERROR! Index out of range from within the 'drive_robot' function in ball_chaser package, 'process_image.cpp' file.");
                 return;
             }
 
-            int r = (int)img.data[index];   // untested (indexing is wrong...)
-            int g = (int)img.data[index+1]; // untested
-            int b = (int)img.data[index+2]; // untested
+            int r = (int)img.data[index];
+            int g = (int)img.data[index+1];
+            int b = (int)img.data[index+2];
 
             if(r == 255 and g == 255 and b == 255)
             {
@@ -72,30 +74,22 @@ void process_image_callback(const sensor_msgs::Image img)
         }    
     }
 
-    // calculate x_bar
+    // find the area centroid of the white ball in the x (horizontal) dimension
     if(x_bar_sum <= 0 or num_white_pixels_found <= 0)
     {
-        x_bar = -1;  // null value for area centroid (white ball was NOT in image frame)
+        x_bar = -1;  // null value for area centroid (white ball was NOT found in image frame)
         lin_x = 0.0; // halt forward movement
         ang_z = 0.0; // halt rotational movement
     }
     else
     {
-        x_bar = x_bar_sum / num_white_pixels_found; // area centroid (white ball was found in image frame)
+        x_bar = x_bar_sum / num_white_pixels_found; // calculate area centroid (white ball was found in image frame)
         // transform from [0, 2400] to [1, -1]: ang_z = (-2.0 / 2400.0) * x_bar + 1.0
         lin_x = 0.3;
         ang_z = m * (float)x_bar + 1.0;
     }
 
     drive_robot(lin_x, ang_z);
-
-
-    // ROS_INFO_STREAM(lin_x);
-    // ROS_INFO_STREAM(ang_z);
-    // ROS_INFO_STREAM("---");
-
-    
-
 
     /*
         Image Coordinates:
@@ -108,22 +102,24 @@ void process_image_callback(const sensor_msgs::Image img)
     */
 
 
-    //ROS_INFO("---------");
-    //ROS_INFO_STREAM(typeid(img.data).name());     // St6vectorIhSaIhEE (aka: vector<unsigned char>)
-    //ROS_INFO_STREAM(typeid(img.data[0]).name());  // h (aka: unsigned char, so cast to int before comparison with 'white_pixel')
-    //ROS_INFO_STREAM(img.data.size());             // 1920000
-    //ROS_INFO_STREAM(img.height);                  // 800
-    //ROS_INFO_STREAM(img.width);                   // 800
-    //ROS_INFO_STREAM(img.step);                    // 2400
-    //ROS_INFO_STREAM((int)img.data[1919998]);      // 155 (when white ball is NOT in bottom right corner of image)
-    //ROS_INFO_STREAM((int)img.data[1919998]);      // 255 (when white ball IS in bottom right corner of image)
-    
-    // // when the blue ball covers the top left pixel:
+    // // debugging notes:
+    // ROS_INFO("---------");
+    // ROS_INFO_STREAM(typeid(img.data).name());     // St6vectorIhSaIhEE (aka: vector<unsigned char>)
+    // ROS_INFO_STREAM(typeid(img.data[0]).name());  // h (aka: unsigned char, so cast to int before comparison with 'white_pixel')
+    // ROS_INFO_STREAM(img.data.size());             // 1920000
+    // ROS_INFO_STREAM(img.height);                  // 800
+    // ROS_INFO_STREAM(img.width);                   // 800
+    // ROS_INFO_STREAM(img.step);                    // 2400
+     
+    // // when a blue ball covers the top left pixel:
     // ROS_INFO_STREAM((int)img.data[0]);           // 0   (red)
-    // ROS_INFO_STREAM((int)img.data[1]);           // 0   (blue)
-    // ROS_INFO_STREAM((int)img.data[2]);           // 255 (green)
+    // ROS_INFO_STREAM((int)img.data[1]);           // 0   (green)
+    // ROS_INFO_STREAM((int)img.data[2]);           // 255 (blue)
+
+    // ROS_INFO_STREAM(lin_x);
+    // ROS_INFO_STREAM(ang_z);
     
-    //S_INFO("---------");
+    // ROS_INFO("---------");
 
 }
 
